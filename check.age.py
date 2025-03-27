4801fe0d9254b2aac20f609a46337565b09dabcc
PrinceeChaudhary <princeechaudhary557@gmail.com>
2025-03-27 10:03:36 +0000

    age

---
+++ check.age.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your name: ")
+age = int( input ("enter your age: "))
+if (age >= 18):
+    print(f"Welcome to our website {first_name}")
+else:
+    print("Our game is not suitable for players under the age of 18")
+
