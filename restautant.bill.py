956729a814fa62f1c398d7c27eb217e742940142
PrinceeChaudhary <princeechaudhary557@gmail.com>
2025-04-02 07:08:08 +0000

    homework

---
+++ restautant.bill.py
@@ -0,0 +1,13 @@
+user_choice = int (input("Enter your choice: "))
+if (user_choice == press1):
+ print(" Burger :$10")
+elif (user_choice == press2):
+ print("pizza :$12")
+elif (user_choice == press3):
+ print ("sushi :$15")
+else :
+    print ("invalid number")
+number_guests = int (input("please enter the number of guests:"))
+if user_choice == 1:
+      total_price = number_guests * 10
+
