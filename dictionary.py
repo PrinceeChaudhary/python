1571af01541d1c26bd86a4fd4847f525d50524eb
PrinceeChaudhary <princeechaudhary557@gmail.com>
2025-04-03 10:28:25 +0000

    lottery

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+student = {
+    "first_name": "Princee",
+    "last_name": "Chaudhary",
+    "age": 19,
+    print("first_name:",student["first_name"])
+    print(f"last_name:" {student["last_name"]})
+    print(f"student age: {student["age"]}")
+    
+}
---
+++ games.py
@@ -0,0 +1,18 @@
+games = [
+    {"name": "PUBG": "editor", "Krafton": "2017"}
+
+]
+ new_game = {
+  "name": input ("Enter the game:"),
+  "editor": input ("Enter the editor:"),
+  "year": input ("Enter the year:"),
+
+
+ }
+
+games.append(new game)
+
+for game in games:
+ print("games:",games["name"])
+ print("games:",games["editor"])
+ print("games:",games["year"])
